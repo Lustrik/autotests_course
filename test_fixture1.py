4c91cfe1ac7dc36b92bc44a2acd44b0bcb7f9ae3
katya <orlova.eo@gmail.com>
2025-11-12 14:01:56 +0300

    fixtures tests

--- test_fixture1.py
+++ test_fixture1.py
@@ -1,46 +1,52 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 link = "http://selenium1py.pythonanywhere.com/"
 
 
 class TestMainPage1():
 
-    @classmethod
-    def setup_class(self):
-        print("\nstart browser for test suite..")
+    @classmethod #декоратор, использованный для удобства чтения кода. Так мы дополнительно размечаем в коде, что метод ниже (в нашем примере с *_class) применяется ко всему классу
+    def setup_class(self): #выполняется один раз перед запуском всех тестовых методов в классе
+        print("\nstart browser for test suite 1..")
         self.browser = webdriver.Chrome()
 
     @classmethod
-    def teardown_class(self):
-        print("quit browser for test suite..")
+    def teardown_class(self): #выполняется один раз после всех тестовых методов в классе
+        print("quit browser for test suite 1..")
         self.browser.quit()
 
     def test_guest_should_see_login_link(self):
+        print('start test link 1')
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        #time.sleep(2)
 
     def test_guest_should_see_basket_link_on_the_main_page(self):
+        print('start test basket 1')
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+        #time.sleep(2)
 
 
 class TestMainPage2():
 
-    def setup_method(self):
-        print("start browser for test..")
+    def setup_method(self): #выполняется перед запуском каждого тестового метода в классе
+        print("start browser for test 2..")
         self.browser = webdriver.Chrome()
 
-    def teardown_method(self):
-        print("quit browser for test..")
+    def teardown_method(self): #выполняется каждый раз после каждого тестового метода в классе
+        print("quit browser for test 2..")
         self.browser.quit()
 
     def test_guest_should_see_login_link(self):
+        print('start test link 2')
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        #time.sleep(10)
 
     def test_guest_should_see_basket_link_on_the_main_page(self):
+        print('start test basket 2')
         self.browser.get(link)
         self.browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
-
-
-
+        #time.sleep(10)
--- test_les3_2_step13.py
+++ test_les3_2_step13.py
@@ -2,81 +2,53 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import unittest
-
-#try: 
     
 class TestStep13(unittest.TestCase):
-    def testStep13(self):
-        link = "http://suninjuly.github.io/registration1.html"
-        browser = webdriver.Chrome()
-        browser.get(link)
+     
+    def testStep1(self):
+        self.stepTemplate("http://suninjuly.github.io/registration1.html")
+    def testStep2(self):
+        self.stepTemplate("http://suninjuly.github.io/registration2.html")
 
-    # код, который заполняет обязательные поля
+    def stepTemplate(self, link):
+        try:
+        
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # код, который заполняет обязательные поля
+        
+            input1 = browser.find_element(By.XPATH,"//input[contains(@class, 'first') and @required]")
+            input1.send_keys("test")
+
+            input2 = browser.find_element(By.XPATH,"//input[contains(@class, 'second') and @required]")
+            input2.send_keys("test")
+
+            input3 = browser.find_element(By.XPATH,"//input[contains(@class, 'third') and @required]")
+            input3.send_keys("test")
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+            print(welcome_text)
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Some shit happened")
     
-        input1 = browser.find_element(By.XPATH,"//input[contains(@class, 'first') and @required]")
-        input1.send_keys("test")
-
-        input2 = browser.find_element(By.XPATH,"//input[contains(@class, 'second') and @required]")
-        input2.send_keys("test")
-
-        input3 = browser.find_element(By.XPATH,"//input[contains(@class, 'third') and @required]")
-        input3.send_keys("test")
-
-        # Отправляем заполненную форму
-        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-        button.click()
-
-        # Проверяем, что смогли зарегистрироваться
-        # ждем загрузки страницы
-        time.sleep(1)
-
-        # находим элемент, содержащий текст
-        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
-        welcome_text = welcome_text_elt.text
-        print(welcome_text)
-
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Some shit happened")
-
-    def testStep14(self):
-        link = "http://suninjuly.github.io/registration2.html"
-        browser = webdriver.Chrome()
-        browser.get(link)
-
-    # код, который заполняет обязательные поля
-    
-        input1 = browser.find_element(By.XPATH,"//input[contains(@class, 'first') and @required]")
-        input1.send_keys("test")
-
-        input2 = browser.find_element(By.XPATH,"//input[contains(@class, 'second') and @required]")
-        input2.send_keys("test")
-
-        input3 = browser.find_element(By.XPATH,"//input[contains(@class, 'third') and @required]")
-        input3.send_keys("test")
-
-        # Отправляем заполненную форму
-        button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-        button.click()
-
-        # Проверяем, что смогли зарегистрироваться
-        # ждем загрузки страницы
-        time.sleep(1)
-
-        # находим элемент, содержащий текст
-        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-        # записываем в переменную welcome_text текст из элемента welcome_text_elt
-        welcome_text = welcome_text_elt.text
-        print(welcome_text)
-
-        # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-        self.assertEqual("Congratulations! You have successfully registered!", welcome_text, "Some shit happened")
-    
-#finally:
-    # ожидание чтобы визуально оценить результаты прохождения скрипта
-        time.sleep(10)
-        # закрываем браузер после всех манипуляций
-        browser.quit()
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(10)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
 
 if __name__ == "__main__":
     unittest.main()
